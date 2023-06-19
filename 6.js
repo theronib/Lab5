05744a81afd9409b1f6a995380f3a5df189fa6c6
theronib <yaroslavcharlies@gmail.com>
2023-06-19 17:57:37 +0300

    Version 1.3

--- 6.js
+++ 6.js
@@ -1,26 +1,19 @@
 // КАТЕГОРІЯ ВЕГА
-// Отримуємо посилання на кнопку "Вега"
 var filterButton = document.querySelector('.pizza-6');
 
-// Отримуємо посилання на всі елементи меню піци
 var menuItems = document.querySelectorAll('.menu-1, .menu-2, .menu-3, .menu-4, .menu-5'); 
 
-// Встановлюємо обробник події при натисканні на кнопку "Вега"
 filterButton.addEventListener('click', function() {
-  // Перебираємо всі елементи меню піци
   for (var i = 0; i < menuItems.length; i++) {
     var menuItem = menuItems[i];
 
-    // Перевіряємо, чи елемент має клас "menu-3" (піца Вега)
     if (menuItem.classList.contains('menu-3')) {
-      // Залишаємо видимим тільки піцу Вега, решта піц приховуємо
       menuItem.style.display = 'block';
     } else {
       menuItem.style.display = 'none';
     }
   }
 });
-
 
 
 // КАТЕГОРІЯ МОРЕПРОДУКТИ
@@ -57,19 +50,30 @@ mushroomButton.addEventListener('click', function() {
   }
 });
 
-// КАТЕГОРІЯ МЯСНІ
-// var meatButton = document.querySelector('.pizza-2'); // Оновіть клас кнопки, якщо потрібно
-// var menuItems = document.querySelectorAll('.menu-1.menu-2, .menu-1.menu-4'); // Оновіть класи меню, якщо потрібно
+// КАТЕГОРІЯ УСІ
+var allButton = document.querySelector('.pizza-1'); // Оновіть клас кнопки, якщо потрібно
+var menuItems = document.querySelectorAll('.menu-1, .menu-2, .menu-3, .menu-4, .menu-5'); // Оновіть класи, якщо потрібно
 
-// meatButton.addEventListener('click', function() {
-//   for (var i = 0; i < menuItems.length; i++) {
-//     var menuItem = menuItems[i];
+allButton.addEventListener('click', function() {
+  for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.style.display = 'block';
+  }
+});
 
-//     if (menuItem.classList.contains('menu-2') && menuItem.classList.contains('menu-4')) {
-//       menuItem.style.display = 'block';
-//     } else {
-//       menuItem.style.display = 'none';
-//     }
-//   }
-// });
 
+// КАТЕГОРІЯ МЯСНІ 
+var meatButton = document.querySelector('.pizza-2');
+var menuItems = document.querySelectorAll('.menu-1, .menu-2, .menu-3, .menu-4, .menu-5'); // Оновіть класи, якщо потрібно
+
+meatButton.addEventListener('click', function() {
+  for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+
+    if (menuItem.querySelector('.menu-type').textContent === "М'ясна піца") {
+      menuItem.style.display = 'block';
+    } else {
+      menuItem.style.display = 'none';
+    }
+  }
+});
